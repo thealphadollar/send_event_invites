59bb4c0f3552a3e1bc956342380016bab10c0bb8
thealphadollar <shivam.cs.iit.kgp@gmail.com>
2018-05-12 02:48:25 +0530

    Complete read_csv function, add instruction to download csv from google groups.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# send_event_invites
+# Send Event Invites 
 A  CLI tool for sending event invitations to a large number of attendees.
+
+## Get CSV file from Google Group
+
+To export your Groups list into a .csv file:</br>
+- Sign-in to Google Groups
+- Click My Groups > check the name of the group
+- In the upper right corner, click Manage
+- Near the top right corner of the page, click Export Members
+
--- src/insert_event.py
+++ src/insert_event.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+import csv
 
 
 def protect(*protected):
@@ -14,12 +15,26 @@ def protect(*protected):
             if meta.has_base:
                 for attribute in attrs:
                     if attribute in protected:
-                        raise AttributeError('Overriding of attribute "%s" not allowed.'%attribute)
+                        raise AttributeError('Overriding of attribute "%s" not allowed.' % attribute)
             meta.has_base = True
             klass = super().__new__(meta, name, bases, attrs)
             return klass
 
     return Protect
+
+
+def find_email_index(csv_data):
+    """
+    Returns the column index for a row at which email address is stored.
+    :param csv_data: data extracted by csv
+    :return:
+        row_index: index till which labels are there, used to skip to data
+        col_index: index of the email address column
+    """
+    for row_index, row in enumerate(csv_data):
+        for col_index, label in enumerate(row):
+            if label == "Email address":
+                return row_index, col_index
 
 
 class InsertEvent(ABC, metaclass=protect("read_csv", "create_event")):
@@ -28,19 +43,27 @@ class InsertEvent(ABC, metaclass=protect("read_csv", "create_event")):
     A new calendar AP can be easily added by creating another derived class and overriding the abstract methods.
     """
 
-    # variable to store event
+    # variables to store event and attendees' email addresses
     event = {}
     attendees = []
 
     def read_csv(self, file_path):
         """
         Reads a csv file and appends the email addresses of attendees to attendees.
+        Currently compatible with any csv file having parameter "Email address" as topic above addresses.
         The method performs a regex check and discards wrongly formatted email addresses.
         :param file_path: path to csv file (default is "./addresses.csv")
         :return:
             Appends the email addresses to the attendees list
         """
-        pass
+
+        with open(file_path) as csv_file:
+            csv_data = csv.reader(csv_file, delimiter=',')
+            skip_row_upto, email_index = find_email_index(csv_data)
+
+            for row_index, row in enumerate(csv_data):
+                if row_index > skip_row_upto:
+                    self.attendees.append(row[email_index])
 
     def create_event(self, event_data):
         """
@@ -58,6 +81,7 @@ class InsertEvent(ABC, metaclass=protect("read_csv", "create_event")):
         :return:
             None
         """
+        # To be implemented in the derived class.
         pass
 
     @abstractmethod
@@ -67,4 +91,5 @@ class InsertEvent(ABC, metaclass=protect("read_csv", "create_event")):
         :return:
             None
         """
+        # To be implemented in the derived class.
         pass
